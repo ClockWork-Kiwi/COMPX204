7ae54144db6a863f9df4e8451727a6e576f655c5
Jesse Wilshier <jessewilshier98@gmail.com>
2018-08-13 00:06:45 +1200

    Finished this assignment

--- prac2/HttpServer.java
+++ prac2/HttpServer.java
@@ -2,34 +2,32 @@ import java.net.*;
 import java.io.*;
 import java.util.*;
 
-class HttpServer
-{
-	public static void main(String args[])
-	{
-		System.out.println("Web Server Starting");
-		int port = 8080;
-		try
-		{
-			ServerSocket server = new ServerSocket(port);
-			while (true)
-			{
-				System.out.println("Waiting for connection...");
-				Socket client = server.accept();				
-				HttpServerSession sesh = new HttpServerSession(client);				
-				System.out.println("Connection accepted");
-				InetAddress IP = client.getInetAddress();
-				String address = IP.getHostAddress();
-				System.out.println("IP: " + address);
-				sesh.run();
-				//Done with the client
-				//client.close();
-			}
-		}
-		catch (Exception e)
-		{
-			System.out.println(e.toString());
-		}
-	}
+class HttpServer {
+    public static void main(String args[]) {
+        System.out.println("Web Server Starting");
+        int port = 8080;
+        if (args.length != 1) {
+            System.out.println("Please specify a valid filepath");
+        } else {
+            try {
+                ServerSocket server = new ServerSocket(port);
+                while (true) {
+                    System.out.println("Waiting for connection...");
+                    Socket client = server.accept();
+                    HttpServerSession sesh = new HttpServerSession(client);
+                    System.out.println("Connection accepted");
+                    InetAddress IP = client.getInetAddress();
+                    String address = IP.getHostAddress();
+                    System.out.println("IP: " + address);
+                    sesh.run(args[0]);
+                    //Done with the client
+                    client.close();
+                }
+            } catch (Exception e) {
+                System.out.println(e.toString());
+            }
+        }
+    }
 }
 				 
 				
--- prac2/HttpServerSession.java
+++ prac2/HttpServerSession.java
@@ -2,95 +2,75 @@ import java.net.*;
 import java.io.*;
 import java.util.*;
 
-class HttpServerSession extends Thread
-{
-	private Socket socket;
+class HttpServerSession extends Thread {
+    private Socket socket;
 
-	public HttpServerSession(Socket accepted)
-	{
-		socket = accepted;
-	}
+    public HttpServerSession(Socket accepted) {
+        socket = accepted;
+    }
 
-	public void run()
-	{
-		try
-		{
-			BufferedOutputStream writer = new BufferedOutputStream(socket.getOutputStream());
-			BufferedReader reader = new BufferedReader( new InputStreamReader(socket.getInputStream())); 
-			String request = reader.readLine();
-			String[] parts = request.split(" ");
-			System.out.println(parts[1].substring(1));
-			if (parts.length != 3)
-			{
+    public void run(String filePath) {
+        try {
+            BufferedOutputStream writer = new BufferedOutputStream(socket.getOutputStream());
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String request = reader.readLine();
+            String[] parts = request.split(" ");
+            //System.out.println(parts[1].substring(1));
+            if (parts.length != 3) {
 
-			}			
-			else
-			{
-				String line;
-				while(true)
-				{
-					line = reader.readLine();
-					if (line == null)
-					{
-						//handle end of file
-					}
+            } else {
+                String line;
+                while (true) {
+                    line = reader.readLine();
+                    if (line == null) {
+                        //handle end of file
+                    }
 
-					if (line.compareTo("") == 0)
-					{
-						break;
-					}
-				}
-				if (parts[0].compareTo("GET") == 0)
-				{					
+                    if (line.compareTo("") == 0) {
+                        break;
+                    }
+                }
+                if (parts[0].compareTo("GET") == 0) {
+                    String fileName = parts[1].substring(1);
+                    File tmpDir = new File(filePath + fileName);
+                    //File tmpDir = new File("/Users/GGPC/Desktop/Uni Work/204/COMPX204/prac2/" + fileName);
+                    if (tmpDir.exists()) {
+                        FileInputStream fileReader = new FileInputStream(fileName);
+                        println(writer, "HTTP/1.1 200 OK");
+                        println(writer, "");
+                        byte[] array = new byte[1024];
+                        int something = 0;
+                        while ((something = fileReader.read(array)) != -1) {
+                            writer.write(array, 0, something);
+                            //Thread.sleep(100);
+                        }
+                        fileReader.close();
+                    } else {
 
-				
-										
-					String fileName = parts[1].substring(1);					
-					File tmpDir = new File("/Desktop/Semester_B/204/COMPX204/prac2" + fileName);
-					//if (tmpDir.exists())
-					//{						
-					FileInputStream fileReader = new FileInputStream(fileName);
-					
-					println(writer, "HTTP/1.1 200 OK");
-					println(writer, "");	
-					byte[] array = new byte[1024];
-					int something = 0;
-					while((something = fileReader.read(array)) != -1)
-					{
-						writer.write(array, 0, something);
-					}					
-					fileReader.close();
-					writer.flush();	
-					writer.close();
-					socket.close();
-					//println(writer, "Hello World");
-					//}
-					//else
-					//{
-					//	println(writer, "HTTP/1.1 404 Not Found");
-					//}					
-				}
-			}
-			
-		}
-		catch (Exception e)
-		{
-			System.out.println(e.toString());		
-		}
-	}
+                        println(writer, "HTTP/1.1 404 Not Found");
+                        println(writer, "");
+                        println(writer, "Error 404: File Not Found");
+                    }
+                    writer.flush();
+                    writer.close();
+                    socket.close();
+                }
+            }
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+    }
 
-	private void println(BufferedOutputStream writer, String message)
-		throws IOException
-	{
+    private void println(BufferedOutputStream writer, String message)
+            throws IOException {
 
-		String toSend = message + "\r\n";
-		byte[] array = toSend.getBytes();
-		for (int i = 0; i < array.length; i++)
-		{
-			writer.write(array[i]);
-		}
-		//System.out.println(toSend);
-		return;
-	}
+        String toSend = message + "\r\n";
+        byte[] array = toSend.getBytes();
+        for (int i = 0; i < array.length; i++) {
+            writer.write(array[i]);
+        }
+        //System.out.println(toSend);
+        return;
+    }
 
 }
